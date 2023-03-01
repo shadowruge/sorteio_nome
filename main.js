cfecb1454a98aad930d2660459825307c2d8d323
shadowruge <zambelestar@gmail.com>
2023-02-28 23:22:48 -0300

    sorteio de nomes

---
+++ main.js
@@ -0,0 +1,41 @@
+function importar() {
+    const input = document.getElementById("file");
+    const file = input.files[0];
+    const reader = new FileReader();
+    reader.onload = function(event) {
+      const nomes = event.target.result.split(",");
+      const indice = Math.floor(Math.random() * nomes.length);
+      const resultado = nomes[indice].trim();
+      
+      // Oculta a seção de dados e exibe a seção de transição
+      document.getElementById("dados").style.display = "none";
+      document.getElementById("transicao").style.display = "block";
+  
+      // Define um temporizador para simular o processamento do sorteio
+      setTimeout(function() {
+        // Oculta a seção de transição e exibe o resultado do sorteio
+        document.getElementById("transicao").style.display = "none";
+        document.getElementById("resultado").innerText = `O nome sorteado é: ${resultado}`;
+        document.getElementById("dados").style.display = "block";
+      }, 3000); // tempo de espera de 3 segundos (3000 milissegundos)
+    };
+    reader.readAsText(file,'utf8');
+  }
+  
+  function sortear() {
+    const nomes = document.getElementById("names").value.split(",");
+    const indice = Math.floor(Math.random() * nomes.length);
+    const resultado = nomes[indice].trim();
+  
+     // Oculta a seção de dados e exibe a seção de transição
+     document.getElementById("dados").style.display = "none";
+     document.getElementById("transicao").style.display = "block";
+    // Define um temporizador para simular o processamento do sorteio
+    setTimeout(function() {
+        // Oculta a seção de transição e exibe o resultado do sorteio
+        document.getElementById("transicao").style.display = "none";
+        document.getElementById("resultado").innerText = `O nome sorteado é: ${resultado}`;
+        document.getElementById("dados").style.display = "block";
+      }, 3000); // tempo de espera de 3 segundos (3000 milissegundos)
+    };
+  
